95cfe30eb16ae57ac19413200dbd0fa9e02e5381
Artem <artem30801@gmail.com>
2021-07-29 16:34:50 +0300

    Added UserFlags

--- discord_snakes/models/enums.py
+++ discord_snakes/models/enums.py
@@ -115,4 +115,32 @@ class Intents(DistinctMixin, IntFlag, metaclass=DistinctFlag):
         for key in kwargs:
             if kwargs[key]:
                 base = base | getattr(cls, key)
-        return cls(base)
+        return base
+
+
+class UserFlags(DistinctMixin, IntFlag, metaclass=DistinctFlag):
+    # Intents defined by Discord API
+    DISCORD_EMPLOYEE = 1 << 0
+    PARTNERED_SERVER_OWNER = 1 << 1
+    HYPESQUAD_EVENTS = 1 << 2
+    BUG_HUNTER_LEVEL_1 = 1 << 3
+
+    HOUSE_BRAVERY = 1 << 6
+    HOUSE_BRILLIANCE = 1 << 7
+    HOUSE_BALANCE = 1 << 8
+    EARLY_SUPPORTER = 1 << 9
+    TEAM_USER = 1 << 10
+
+    BUG_HUNTER_LEVEL_2 = 1 << 14
+
+    VERIFIED_BOT = 1 << 16
+    EARLY_VERIFIED_BOT_DEVELOPER = 1 << 17
+    DISCORD_CERTIFIED_MODERATOR = 1 << 18
+
+    # Shortcuts/grouping/aliases
+    HYPESQUAD = HOUSE_BRAVERY | HOUSE_BRILLIANCE | HOUSE_BALANCE
+    BUG_HUNTER = BUG_HUNTER_LEVEL_1 | BUG_HUNTER_LEVEL_2
+
+    # Special members
+    none = 0
+    ALL = AntiFlag()
