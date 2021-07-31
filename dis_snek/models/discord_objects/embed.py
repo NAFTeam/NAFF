064f1f2177780a2d4abf9dd0f08f0e212efddf47
LordOfPolls <ddavidallen13@gmail.com>
2021-07-31 19:42:20 +0100

    re-make embed.py with attrs

--- dis_snek/models/discord_objects/embed.py
+++ dis_snek/models/discord_objects/embed.py
@@ -1,97 +1,155 @@
-from typing import List
+"""
+The MIT License (MIT).
+
+Copyright (c) 2021 - present LordOfPolls
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+from typing import Any
 from typing import Optional
+
+import attr
+from attr.validators import instance_of
+from attr.validators import optional
 
 from dis_snek.models.timestamp import Timestamp
 
 
-class Embed:
-    def __init__(self, name: Optional[str] = None, color: Optional[int] = None, description: Optional[str] = None):
-        self.name: Optional[str] = name
-        self.color: Optional[int] = color
-        self.description: Optional[str] = description
+@attr.s(slots=True)
+class Embed(object):
+    """Represents a discord embed object.
 
-        self.url: Optional[str] = None
-        self.timestamp: Optional[Timestamp] = None
-        self.footer: Optional[dict] = None
-        self.image: Optional[dict] = None
-        self.thumbnail: Optional[dict] = None
-        self.video: Optional[dict] = None
-        self.provider: Optional[dict] = None
-        self.author: Optional[dict] = None
-        self.fields: Optional[List[dict]] = []
+    :param title: the title of the embed
+    :param description: the description of the embed
+    :param color: the color of the embed
+    """
 
-    def __setattr__(self, key, value):
-        if key == "title":
-            if len(value) > 256:
-                raise ValueError("Title cannot exceed 256 characters")
-        elif key == "description":
-            if len(value) > 4096:
-                raise ValueError("Description cannot exceed 4096 characters")
-        super().__setattr__(key, value)
+    title: Optional[str] = attr.ib(default=None)
+    description: Optional[str] = attr.ib(default=None)
+    color: Optional[str] = attr.ib(default=None)
 
-    def to_dict(self):
+    url: Optional[str] = attr.ib(validator=optional(instance_of(str)), default=None, init=False)
+    timestamp: Optional[Timestamp] = attr.ib(validator=optional(instance_of(Timestamp)), default=None, init=False)
+    footer: Optional[dict] = attr.ib(default=None, init=False)
+    image: Optional[dict] = attr.ib(default=None, init=False)
+    thumbnail: Optional[dict] = attr.ib(default=None, init=False)
+    video: Optional[dict] = attr.ib(default=None, init=False)
+    provider: Optional[dict] = attr.ib(default=None, init=False)
+    author: Optional[dict] = attr.ib(default=None, init=False)
+    fields: list = attr.ib(factory=list)
+
+    @title.validator
+    def _name_validation(self, attribute: str, value: Any) -> None:
+        """Validate the embed title."""
+        if value is not None:
+            if isinstance(value, str):
+                if len(value) > 256:
+                    raise ValueError("Title cannot exceed 256 characters")
+                return
+            raise TypeError("Title must be of type String")
+
+    @description.validator
+    def _description_validation(self, attribute: str, value: Any) -> None:
+        """Validate the description."""
+        if value is not None:
+            if isinstance(value, str):
+                if len(value) > 4096:
+                    raise ValueError("Description cannot exceed 4096 characters")
+                return
+            raise TypeError("Description must be of type String")
+
+    @fields.validator
+    def _fields_validation(self, attribute: str, value: Any) -> None:
+        """Validate the fields."""
+        if isinstance(value, list):
+            if len(value) > 25:
+                raise ValueError("Embeds can only hold 25 fields")
+
+    def to_dict(self) -> dict:
         """
-        Convert this object into a dict ready for discord
+        Convert this object into a dict ready for discord.
+
         :return: dict
         """
-        if len(self.fields) > 25:
-            raise ValueError("Embeds can only hold 25 fields")
+        self._name_validation("title", self.title)
+        self._description_validation("description", self.description)
+        self._fields_validation("fields", self.fields)
+
         if len(self) > 6000:
             raise ValueError(
-                "Your embed is too large, please view https://discord.com/developers/docs/resources/channel#embed-limits"
+                "Your embed is too large, go to https://discord.com/developers/docs/resources/channel#embed-limits"
             )
-        data = self.__dict__
         # lets be nice and remove all the None values
-        return {key: value for key, value in data.items() if value}
+        return attr.asdict(self, filter=lambda k, v: v)
 
     def __len__(self):
         # yes i know there are far more optimal ways to write this
         # its written like this for readability
         total = 0
-        total += len(self.name) if self.name else 0
+        total += len(self.title) if self.title else 0
         total += len(self.description) if self.description else 0
         total += len(self.footer.get("text")) if self.footer else 0
-        total += len(self.author.get("name")) if self.author else 0
+        total += len(self.author.get("title")) if self.author else 0
 
         for field in self.fields:
-            total += len(field["name"]) + len(field["value"])
+            total += len(field["title"]) + len(field["value"])
         return total
 
-    def set_thumbnail(self, url: str):
+    def set_thumbnail(self, url: str) -> None:
         """
-        Set the thumbnail of the embed
+        Set the thumbnail of the embed.
+
         :param url: the url of the image to use
         """
         self.thumbnail = {"url": url}
 
-    def set_image(self, url: str):
+    def set_image(self, url: str) -> None:
         """
-        Set the image of the embed
+        Set the image of the embed.
+
         :param url: the url of the image to use
         """
         self.image = {"url": url}
 
-    def set_author(self, name: str, icon_url: Optional[str] = None):
+    def set_author(self, name: str, icon_url: Optional[str] = None) -> None:
         """
-        Set the author field of the embed
-        :param name: The text to go in the name section
+        Set the author field of the embed.
+
+        :param name: The text to go in the title section
         :param icon_url: A url of an image to use as the icon
         """
-        self.author = {"name": name, "icon_url": icon_url}
+        self.author = {"title": name, "icon_url": icon_url}
 
-    def set_footer(self, text: str, icon_url: Optional[str] = None):
+    def set_footer(self, text: str, icon_url: Optional[str] = None) -> None:
         """
-        Set the footer field of the embed
-        :param text: The text to go in the name section
+        Set the footer field of the embed.
+
+        :param text: The text to go in the title section
         :param icon_url: A url of an image to use as the icon
         """
         self.footer = {"text": text, "icon_url": icon_url}
 
-    def add_field(self, name: str, value: str, inline: bool = False):
+    def add_field(self, name: str, value: str, inline: bool = False) -> None:
         """
-        Add a field to the embed
-        :param name: The name of this field
+        Add a field to the embed.
+
+        :param name: The title of this field
         :param value: The value in this field
         :param inline: Should this field be inline with other fields?
         """
-        self.fields.append({"name": name, "value": value, "inline": inline})
+        self.fields.append({"title": name, "value": value, "inline": inline})
+        self._fields_validation("fields", self.fields)
