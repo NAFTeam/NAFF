99cff27abec4304025854ff726d89f9aec75119c
Artem <artem30801@gmail.com>
2021-08-01 16:22:21 +0300

    Added attr_utils

---
+++ dis_snek/utils/attr_utils.py
@@ -0,0 +1,22 @@
+import attr
+
+
+def converter(attribute):
+    def decorator(func):
+        attribute.converter = func
+        return staticmethod(func)
+    return decorator
+
+
+class IgnoreExtraKeysMixin:
+    @classmethod
+    def _get_keys(cls):
+        if (keys := getattr(cls, "_keys", None)) is None:
+            keys = frozenset(field.name for field in attr.fields(cls))
+            setattr(cls, "_keys", keys)
+        return keys
+
+    @classmethod
+    def _filter_kwargs(cls, kwargs_dict: dict):
+        keys = cls._get_keys()
+        return {k: v for k, v in kwargs_dict.items() if k in keys}
