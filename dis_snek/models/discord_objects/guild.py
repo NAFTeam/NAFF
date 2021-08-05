b3f82c956dbb86bccdd2dbcc5dd1a00113760b91
Artem <artem30801@gmail.com>
2021-08-05 10:56:02 +0300

    Initial converstion of Guild to use attr

--- dis_snek/models/discord_objects/guild.py
+++ dis_snek/models/discord_objects/guild.py
@@ -1,143 +1,93 @@
 from typing import Dict
 from typing import List
 from typing import Optional
-from typing import TYPE_CHECKING
+from typing import Any
+
+import attr
+from attr.converters import optional as optional_c
 
 from dis_snek.models.discord_objects.channel import BaseChannel
 from dis_snek.models.discord_objects.channel import Thread
 from dis_snek.models.discord_objects.channel import TYPE_ALL_CHANNEL
 from dis_snek.models.snowflake import Snowflake
 from dis_snek.models.snowflake import Snowflake_Type
-
-if TYPE_CHECKING:
-    from dis_snek.client import Snake
-
-
-class BaseGuild(Snowflake):
-    __slots__ = "id", "unavailable", "_client"
-
-    def __init__(self, data, client):
-        self._client: Snake = client
-        self.id: Snowflake_Type = data["id"]
-        self.unavailable: bool = data.get("unavailable", False)
+from dis_snek.utils.attr_utils import default_kwargs
+from dis_snek.utils.attr_utils import DictSerializationMixin
 
 
+@attr.s(**default_kwargs)
+class BaseGuild(Snowflake, DictSerializationMixin):
+    _client: Any = attr.ib()
+    unavailable: bool = attr.ib(default=False)
+
+
+@attr.define(**default_kwargs)
 class Guild(BaseGuild):
-    __slots__ = (
-        "name",
-        "icon",
-        "icon_hash",
-        "splash",
-        "discovery_splash",
-        "is_owner",
-        "owner_id",
-        "permissions",
-        "afk_channel_id",
-        "afk_timeout",
-        "widget_enabled",
-        "widget_channel_id",
-        "verification_level",
-        "default_message_notifications",
-        "explicit_content_filter",
-        "roles",
-        "emojis",
-        "features",
-        "mfa_level",
-        "system_channel_id",
-        "system_channel_flags",
-        "rules_channel_id",
-        "joined_at",
-        "large",
-        "member_count",
-        "voice_states",
-        "members",
-        "_channels",
-        "_threads",
-        "presences",
-        "max_presences",
-        "max_members",
-        "vanity_url_code",
-        "description",
-        "banner",
-        "premium_tier",
-        "premium_sub_count",
-        "preferred_locale",
-        "public_updates_channel_id",
-        "max_video_channel_users",
-        "welcome_screen",
-        "nsfw_level",
-        "stage_instances",
-        "stickers",
-    )
+    name: str = attr.ib()
+    _icon: Optional[str] = attr.ib(default=None)  # todo merge, convert to asset
+    _icon_hash: Optional[str] = attr.ib(default=None)
+    splash: Optional[str] = attr.ib(default=None)
+    discovery_splash: Optional[str] = attr.ib(default=None)
+    is_owner: bool = attr.ib(default=False)
+    owner_id: Snowflake_Type = attr.ib()
+    permissions: Optional[str] = attr.ib(default=None)  # todo convert to permissions obj
+    afk_channel_id: Optional[Snowflake_Type] = attr.ib(default=None)
+    afk_timeout: Optional[int] = attr.ib(default=None)
+    widget_enabled: bool = attr.ib(default=False)
+    widget_channel_id: Optional[Snowflake_Type] = attr.ib(default=None)
+    verification_level: int = attr.ib(default=0)  # todo enum
+    default_message_notifications: int = attr.ib(default=0)  # todo enum
+    explicit_content_filter: int = attr.ib(default=0)  # todo enum
+    _roles: List[dict] = attr.ib(factory=list)
+    _emojis: List[dict] = attr.ib(factory=list)
+    _features: List[str] = attr.ib(factory=list)
+    mfa_level: int = attr.ib(default=0)
+    system_channel_id: Optional[Snowflake_Type] = attr.ib(default=None)
+    system_channel_flags: int = attr.ib(default=0)  # todo enum
+    rules_channel_id: Optional[Snowflake_Type] = attr.ib(default=None)
+    joined_at: str = attr.ib()
+    large: bool = attr.ib(default=False)
+    member_count: int = attr.ib(default=0)
+    voice_states: List[dict] = attr.ib(factory=list)
+    members: List[dict] = attr.ib(factory=list)
+    # _channels: Dict[str, BaseChannel] = {}
+    # _threads: Dict[str, Thread] = {}
+    presences: List[dict] = attr.ib(factory=list)
+    max_presences: Optional[int] = attr.ib(default=None)
+    max_members: Optional[int] = attr.ib(default=None)
+    vanity_url_code: Optional[str] = attr.ib(default=None)
+    description: Optional[str] = attr.ib(default=None)
+    banner: Optional[str] = attr.ib(default=None)
+    premium_tier: Optional[str] = attr.ib(default=None)
+    premium_subscription_count: int = attr.ib(default=0)
+    preferred_locale: str = attr.ib()
+    public_updates_channel_id: Optional[Snowflake_Type] = attr.ib(default=None)
+    max_video_channel_users: int = attr.ib(default=0)
+    welcome_screen: Optional[dict] = attr.ib(factory=list)
+    nsfw_level: int = attr.ib(default=0)  # todo enum
+    stage_instances: List[dict] = attr.ib(factory=list)
+    stickers: List[dict] = attr.ib(factory=list)
 
-    def __init__(self, data: dict, client):
-        super().__init__(data, client)
+    # if not self.member_count and "approximate_member_count" in data:
+    #     self.member_count = data.get("approximate_member_count", 0)
 
-        self.name: str = data.get("name")
-        self.icon: Optional[str] = data.get("icon")
-        self.icon_hash: Optional[str] = data.get("icon_hash")
-        self.splash: Optional[str] = data.get("splash")
-        self.discovery_splash: Optional[str] = data.get("discovery_splash")
-        self.is_owner: bool = data.get("owner", False)
-        self.owner_id: Snowflake_Type = data.get("owner_id")
-        self.permissions: Optional[str] = data.get("permissions")
-        self.afk_channel_id: Optional[Snowflake_Type] = data.get("afk_channel_id")
-        self.afk_timeout: Optional[int] = data.get("afk_timeout", 0)
-        self.widget_enabled: bool = data.get("widget_enabled", False)
-        self.widget_channel_id: Optional[Snowflake_Type] = data.get("widget_channel_id")
-        self.verification_level: int = data.get("verification_level", 0)
-        self.default_message_notifications: int = data.get("default_message_notifications", 0)
-        self.explicit_content_filter: int = data.get("explicit_content_filter", 0)
-        self.roles: List[{}] = data.get("roles", [])
-        self.emojis: List[{}] = data.get("emojis", [])
-        self.features: List[str] = data.get("features", [])
-        self.mfa_level: int = data.get("mfa_level", 0)
-        self.system_channel_id: Optional[Snowflake_Type] = data.get("system_channel_id")
-        self.system_channel_flags: int = data.get("system_channel_flags", 0)
-        self.rules_channel_id: Optional[Snowflake_Type] = data.get("rules_channel_id")
-        self.joined_at: str = data.get("joined_at")
-        self.large: bool = data.get("large", False)
-        self.member_count: int = data.get("member_count", 0)
-        self.voice_states: List[{}] = data.get("voice_states", 0)
-        self.members: List[{}] = data.get("members", [])
-        self._channels: Dict[str, BaseChannel] = {}
-        self._threads: Dict[str, Thread] = {}
-        self.presences: List[{}] = data.get("presences", [])
-        self.max_presences: Optional[int] = data.get("max_presences")
-        self.max_members: Optional[int] = data.get("max_members")
-        self.vanity_url_code: Optional[str] = data.get("vanity_url_code")
-        self.description: Optional[str] = data.get("description")
-        self.banner: Optional[str] = data.get("banner")
-        self.premium_tier: Optional[str] = data.get("premium_tier")
-        self.premium_sub_count: int = data.get("premium_subscription_count", 0)
-        self.preferred_locale: str = data.get("preferred_locale")
-        self.public_updates_channel_id: Optional[Snowflake_Type] = data.get("public_updates_channel_id")
-        self.max_video_channel_users: int = data.get("max_video_channel_users", 0)
-        self.welcome_screen: Optional[{}] = data.get("welcome_screen", [])
-        self.nsfw_level: int = data.get("nsfw_level", 0)
-        self.stage_instances: List[{}] = data.get("stage_instances", [])
-        self.stickers: List[{}] = data.get("stickers", [])
+    # channels: List[{}] = data.get("channels", [])
+    # threads: List[{}] = data.get("threads", [])
+    #
+    # for c_data in channels:
+    #     _channel = BaseChannel.from_dict(c_data, self._client)
+    #     self._channels[_channel.id] = _channel
+    #
+    # for t_data in threads:
+    #     _channel = BaseChannel.from_dict(t_data, self._client)
+    #     self._threads[_channel.id] = _channel
 
-        if not self.member_count and "approximate_member_count" in data:
-            self.member_count = data.get("approximate_member_count", 0)
-
-        channels: List[{}] = data.get("channels", [])
-        threads: List[{}] = data.get("threads", [])
-
-        for c_data in channels:
-            _channel = BaseChannel.from_dict(c_data, self._client)
-            self._channels[_channel.id] = _channel
-
-        for t_data in threads:
-            _channel = BaseChannel.from_dict(t_data, self._client)
-            self._threads[_channel.id] = _channel
-
-    @property
-    async def channels(self) -> List[TYPE_ALL_CHANNEL]:
-        if not self._channels:
-            # need to acquire channels
-            _channels = await self._client.http.get_channels(self.id)
-
-            for c_data in _channels:
-                self._channels.append(BaseChannel.from_dict(c_data, self._client))
-        return self._channels
+    # @property
+    # async def channels(self) -> List[TYPE_ALL_CHANNEL]:
+    #     if not self._channels:
+    #         # need to acquire channels
+    #         _channels = await self._client.http.get_channels(self.id)
+    #
+    #         for c_data in _channels:
+    #             self._channels.append(BaseChannel.from_dict(c_data, self._client))
+    #     return self._channels
