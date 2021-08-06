1d8c91a7b456814a9a2380ec18826db47a3e9328
Zevaryx <zevaryx@gmail.com>
2021-08-06 13:19:33 -0600

    Add Reaction object

---
+++ dis_snek/models/discord_objects/reaction.py
@@ -0,0 +1,14 @@
+import attr
+
+from dis_snek.models.discord_objects.emoji import Emoji
+from dis_snek.utils.attr_utils import DictSerializationMixin
+
+
+@attr.s(slots=True, kw_only=True)
+class Reaction(DictSerializationMixin):
+    # TODO: custom_emoji, message
+    count: int = attr.ib()
+    me: bool = attr.ib(default=False)
+    emoji: Emoji = attr.ib()  # TODO: partial emoji (name is null)
+
+    # TODO: clear, remove, users
