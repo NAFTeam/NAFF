67bd754cfb16626f5d43689cad7cfff3dc29431c
Zevaryx <zevaryx@gmail.com>
2021-08-14 01:28:30 -0600

    Add pin, unpin, edit endpoints

--- dis_snek/http_requests/messages.py
+++ dis_snek/http_requests/messages.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, List, Optional, Union
 
 from dis_snek.models.route import Route
-from dis_snek.models.snowflake import Snowflake_Type
+from dis_snek.models.snowflake import Snowflake, Snowflake_Type
 
 
 class MessageRequests:
@@ -42,3 +42,47 @@ class MessageRequests:
         :return: message or None
         """
         return await self.request(Route("GET", f"/channels/{channel_id}/messages/{message_id}"))
+
+    async def pin_message(self, channel_id: Snowflake_Type, message_id: Snowflake_Type) -> None:
+        """Pin a message to a channel
+
+        :param channel_id: Channel to pin message to
+        :param message_id: Message to pin
+        """
+        return await self.request(Route("PUT", f"/channels/{channel_id}/pins/{message_id}"))
+
+    async def unpin_message(self, channel_id: Snowflake_Type, message_id: Snowflake_Type) -> None:
+        """Unpin a message to a channel
+
+        :param channel_id: Channel to unpin message to
+        :param message_id: Message to unpin
+        """
+        return await self.request(Route("DELETE", f"/channels/{channel_id}/pins/{message_id}"))
+
+    async def edit_message(
+        self,
+        channel_id: Snowflake_Type,
+        message_id: Snowflake_Type,
+        content: Optional[str],
+        embeds: Optional[List[Dict]] = None,
+        components: Optional[List[dict]] = None,
+    ) -> dict:
+        """Edit an existing message
+
+        :param channel_id: Channel of message to edit
+        :param message_id: Message to edit
+        :param content: Content of the edit
+        :param embeds: Message embeds
+        :param components: Message components
+        :return: Message object of edited message
+        """
+        # TODO: Add support for attachments, file, flags, allowed_mentions
+        payload: Dict[str, Any] = {}
+
+        if content:
+            payload["content"] = content
+        if embeds:
+            payload["embeds"] = embeds
+        if components:
+            payload["components"] = components
+        return await self.request(Route("PATCH", f"/channels/{channel_id}/messages/{message_id}"), json=payload)
