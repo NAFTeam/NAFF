38466f24c55f9d0bf01804f60aecded86631b48e
Zevaryx <zevaryx@gmail.com>
2021-08-14 01:34:29 -0600

    Remove unused Snowflake import

--- dis_snek/http_requests/messages.py
+++ dis_snek/http_requests/messages.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, List, Optional, Union
 
 from dis_snek.models.route import Route
-from dis_snek.models.snowflake import Snowflake, Snowflake_Type
+from dis_snek.models.snowflake import Snowflake_Type
 
 
 class MessageRequests:
