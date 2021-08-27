ebd5cb0f5769331306d13e676c332b1b39570788
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-08-27 14:11:06 +0800

    Add docs for emoji model things.

--- dis_snek/models/discord_objects/emoji.py
+++ dis_snek/models/discord_objects/emoji.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 import attr
 
@@ -7,10 +7,21 @@ from dis_snek.models.route import Route
 from dis_snek.models.snowflake import Snowflake, Snowflake_Type
 from dis_snek.utils.attr_utils import DictSerializationMixin
 
+if TYPE_CHECKING:
+    from dis_snek.client import Snake
+
 
 @attr.s(slots=True)
 class PartialEmoji(Snowflake):
-    id: Optional[Snowflake_Type] = attr.ib(default=None)  # can be None for Standard Emoji
+    """
+    Represent a basic emoji used in discord.
+
+    :param id: The custom emoji id. Leave empty if you are using standard unicode emoji
+    :param name: The custom emoji name. Or standard unicode emoji in string.
+    :param animated: Whether this emoji is animated.
+    """
+
+    id: Optional[Snowflake_Type] = attr.ib(default=None)
     name: Optional[str] = attr.ib(default=None)
     animated: bool = attr.ib(default=False)
 
@@ -20,7 +31,7 @@ class PartialEmoji(Snowflake):
     @property
     def req_format(self) -> str:
         """
-        Format used for web request
+        Format used for web request.
         """
         if self.id:
             return f"{self.name}:{self.id}"
@@ -33,7 +44,18 @@ class PartialEmoji(Snowflake):
 
 @attr.s(slots=True, kw_only=True)
 class Emoji(PartialEmoji, DictSerializationMixin):
-    _client: Any = attr.ib()
+    """
+    Represent a custom emoji in a guild with all its properties.
+
+    :param roles: Roles allowed to use this emoji
+    :param creator: User that made this emoji.
+    :param require_colons: Whether this emoji must be wrapped in colons
+    :param managed: Whether this emoji is managed.
+    :param available: Whether this emoji can be used, may be false due to loss of Server Boosts.
+    :param guild_id: The guild that this custom emoji is created in.
+    """
+
+    _client: "Snake" = attr.ib()
     roles: List[Snowflake] = attr.ib(factory=list)
     creator: Optional[User] = attr.ib(default=None)
 
@@ -52,7 +74,7 @@ class Emoji(PartialEmoji, DictSerializationMixin):
     @property
     def is_usable(self) -> bool:
         """
-        Determines if this emoji is usable by the current user
+        Determines if this emoji is usable by the current user.
         """
         if not self.available:
             return False
@@ -60,6 +82,9 @@ class Emoji(PartialEmoji, DictSerializationMixin):
         return True
 
     async def delete(self, reason: Optional[str] = None) -> None:
+        """
+        Deletes the custom emoji from the guild.
+        """
         if self.guild_id:
             await self._client.http.request(Route("DELETE", f"/guilds/{self.guild_id}/emojis/{self.id}"), reason=reason)
         raise ValueError("Cannot delete emoji, no guild_id set")
