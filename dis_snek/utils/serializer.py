a6731c6efc6a04bfcc121246d311c8dfbeba9ace
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-08-29 23:47:38 +0800

    Dict serialization should exclude empty list and dicts as well.

--- dis_snek/utils/serializer.py
+++ dis_snek/utils/serializer.py
@@ -22,7 +22,7 @@ def to_dict(inst):
 
         raw_value = getattr(inst, a.name)
         value = _to_dict_any(raw_value)
-        if value is not None:
+        if isinstance(value, bool) or value:
             d[a.name] = value
 
     return d
