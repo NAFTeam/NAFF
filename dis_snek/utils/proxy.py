49c8d0f65b0fe49d87092c443907086e066d84aa
Artem <artem30801@gmail.com>
2021-09-04 01:12:09 +0300

    Started proxy rework, added .chain(), .parallel() methods to proxies

--- dis_snek/utils/proxy.py
+++ dis_snek/utils/proxy.py
@@ -1,8 +1,10 @@
 from inspect import isawaitable, iscoroutinefunction
+from functools import wraps
 from operator import getitem
 from typing import TYPE_CHECKING, Union, Awaitable, Callable, Coroutine, Any, List
 
 import attr
+import asyncio
 
 from dis_snek.models.snowflake import to_snowflake
 from dis_snek.utils.attr_utils import copy_converter
@@ -11,8 +13,51 @@ if TYPE_CHECKING:
     from dis_snek.models.snowflake import Snowflake_Type
 
 
+def get_id(obj):
+    try:
+        return obj.id
+    except AttributeError:
+        return to_snowflake(obj)
+
+
+def return_proxy(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        return AsyncPartial(func, self, *args, **kwargs)
+
+    return wrapper
+
+
+async def maybe_await(func, *args, **kwargs):
+    value = func(*args, **kwargs)
+    if isawaitable(value):
+        value = await value
+    return value
+
+
+# @attr.define()
+# class PartialHolder:
+#     args: tuple = attr.field()
+#     kwargs: dict = attr.field()
+#
+#
+# def partial_holder(*args, **kwargs):
+#     return PartialHolder(args, kwargs)
+
+
+class _ProxyViewCalls:
+    def map(self, func):
+        pass
+
+    def reduce(self, func):
+        pass
+
+    def filter(self, func):
+        pass
+
+
 @attr.define()
-class CacheView:
+class CacheView(_ProxyViewCalls):
     ids: Union[Awaitable, Callable[..., Coroutine[Any, Any, Any]], List["Snowflake_Type"]] = attr.field(
         converter=copy_converter
     )
@@ -45,6 +90,7 @@ class CacheView:
             yield await self._method(to_snowflake(instance_id))
 
 
+@attr.define()
 class _BaseProxy:
     def __getattr__(self, item):
         return ValueProxy(self, item, getter=getattr)
@@ -53,10 +99,23 @@ class _BaseProxy:
         return ValueProxy(self, item, getter=getitem)
 
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
-        return CallProxy(self, args, kwargs)
+        return MethodCallProxy(self, args, kwargs)
 
     async def _resolve_proxies(self):
         return await self
+
+    def chain(self, *func_chain):
+        proxy = self
+        for func in func_chain:
+            proxy = SingleCallProxy(proxy, func)
+        return proxy
+
+    def parallel(self, *funcs, parallel=True):
+        """parallel=False -> funcs run independently but in sequence"""
+        return MultiCallProxy(self, funcs, parallel=parallel)
+
+    # def map(self, func):
+    #     pass
 
 
 @attr.define()
@@ -70,6 +129,7 @@ class CacheProxy(_BaseProxy):
 
 @attr.define()
 class ValueProxy(_BaseProxy):
+    """Proxy that is used for name resolution"""
     _proxy: "TYPE_ALL_PROXY" = attr.field()
     _item: Any = attr.field()
     _getter: Callable = attr.field()
@@ -91,7 +151,8 @@ class ValueProxy(_BaseProxy):
 
 
 @attr.define()
-class CallProxy(_BaseProxy):
+class MethodCallProxy(_BaseProxy):
+    """Proxy for method calls"""
     _proxy: "TYPE_ALL_PROXY" = attr.field()
     _args: Any = attr.field(factory=tuple)
     _kwargs: Any = attr.field(factory=dict)
@@ -101,16 +162,13 @@ class CallProxy(_BaseProxy):
 
     async def _call_proxy_method(self):
         method = await self._proxy
-        value = method(*self._args, **self._kwargs)
         # for coroutines and sync methods returning awaitables (methods decorated with @return_proxy)
-        if isawaitable(value):
-            value = await value
-        return value
+        return await maybe_await(method, *self._args, **self._kwargs)
 
 
 @attr.define(init=False)
-class PartialCallableProxy(_BaseProxy):
-    _callable: Callable[..., Coroutine[Any, Any, Any]] = attr.field(repr=False)
+class AsyncPartial(_BaseProxy):
+    _callable: Callable[..., Coroutine[Any, Any, Any]] = attr.field()
     _args: Any = attr.field()
     _kwargs: Any = attr.field()
 
@@ -126,4 +184,43 @@ class PartialCallableProxy(_BaseProxy):
         return await self._callable(*self._args, **self._kwargs)
 
 
-TYPE_ALL_PROXY = Union[CacheProxy, ValueProxy, CallProxy, PartialCallableProxy]
+@attr.define()
+class SingleCallProxy(_BaseProxy):
+    _proxy: "TYPE_ALL_PROXY" = attr.field()
+    _callable: Callable[..., Coroutine[Any, Any, Any]] = attr.field()
+
+    def __await__(self):
+        return self.__call__().__await__()
+
+    async def __call__(self) -> Any:
+        instance = await self._proxy
+        return await maybe_await(self._callable, instance)
+
+
+@attr.define()
+class MultiCallProxy(_BaseProxy):
+    _proxy: "TYPE_ALL_PROXY" = attr.field()
+    _callables: List[Callable[..., Coroutine[Any, Any, Any]]] = attr.field()
+    parallel: bool = attr.field()
+
+    def __await__(self):
+        return self.get_list().__await__()
+
+    async def __call__(self) -> Any:
+        return await self.get_list()
+
+    async def get_list(self):
+        return [instance async for instance in self]
+
+    async def __aiter__(self):
+        instance = await self._proxy
+        if self.parallel:
+            tasks = [maybe_await(func, instance) for func in self._callables]
+            for result in asyncio.as_completed(tasks):
+                yield await result
+        else:
+            for func in self._callables:
+                yield await maybe_await(func, instance)
+
+
+TYPE_ALL_PROXY = Union[CacheProxy, ValueProxy, MethodCallProxy, AsyncPartial, SingleCallProxy, MultiCallProxy]
