fc492b91575b7060bb34587315605945f9e746b0
LordOfPolls <ddavidallen13@gmail.com>
2021-09-04 18:25:35 +0100

    Add is_owner check ++
    also refactored checks

--- dis_snek/models/checks.py
+++ dis_snek/models/checks.py
@@ -5,13 +5,10 @@ def has_role(role_id):
     """
     Checks if the author has a role with a specific id.
     :param coro: the function to check
-    :return:
     """
 
     async def check(ctx: Context) -> bool:
-        if any(role.id == role_id for role in await ctx.author.roles):
-            return True
-        return False
+        return any(role.id == role_id for role in await ctx.author.roles)
 
     return check
 
@@ -20,12 +17,21 @@ def has_id(user_id):
     """
     Checks if the author has the desired ID.
     :param coro: the function to check
-    :return:
     """
 
     async def check(ctx: Context) -> bool:
-        if ctx.author.id == user_id:
-            return True
-        return False
+        return ctx.author.id == user_id
+
+    return check
+
+
+def is_owner():
+    """
+    Is the author the owner of the bot.
+    :param coro: the function to check
+    """
+
+    async def check(ctx: Context) -> bool:
+        return ctx.author.id == await ctx.bot.owner
 
     return check
