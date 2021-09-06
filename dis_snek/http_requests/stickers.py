01b24be1d4947985843bf32e6f0bd98cf50d4ef9
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-09-06 13:20:26 +0800

    Complete sticker endpoints.

--- dis_snek/http_requests/stickers.py
+++ dis_snek/http_requests/stickers.py
@@ -1,8 +1,9 @@
-from typing import TYPE_CHECKING, Any, List
+from typing import TYPE_CHECKING, Any, List, Optional
 
 from dis_snek.models.route import Route
 
 if TYPE_CHECKING:
+    from aiohttp import FormData
     from dis_snek.models.snowflake import Snowflake_Type
 
 
@@ -18,6 +19,23 @@ class StickerRequests:
         """
         return await self.request(Route("GET", f"/stickers/{sticker_id}"))
 
+    async def list_nitro_sticker_packs(self) -> list:
+        """
+        Gets the list of sticker packs available to Nitro subscribers.
+
+        :return: List of sticker packs
+        """
+        return await self.request(Route("GET", "/sticker-packs"))
+
+    async def list_guild_stickers(self, guild_id: "Snowflake_Type") -> List[dict]:
+        """
+        Get the stickers for a guild.
+
+        :param guild_id: The guild to get stickers from
+        :return: List of Stickers or None
+        """
+        return await self.request(Route("GET", f"/guild/{guild_id}/stickers"))
+
     async def get_guild_sticker(self, guild_id: "Snowflake_Type", sticker_id: "Snowflake_Type") -> dict:
         """
         Get a sticker from a guild.
@@ -28,11 +46,39 @@ class StickerRequests:
         """
         return await self.request(Route("GET", f"/guild/{guild_id}/stickers/{sticker_id}"))
 
-    async def get_guild_stickers(self, guild_id: "Snowflake_Type") -> List[dict]:
+    async def create_guild_sticker(self, payload: "FormData", guild_id: "Snowflake_Type", reason: Optional[str] = None):
         """
-        Get the stickers for a guild.
+        Create a new sticker for the guild. Requires the MANAGE_EMOJIS_AND_STICKERS permission.
 
-        :param guild_id: The guild to get stickers from
-        :return: List of Stickers or None
+        :param payload: the payload to send.
+        :param guild_id: The guild to create sticker at.
+        :param reason: The reason for this action.
+
+        :return: The new sticker data on success.
         """
-        return await self.request(Route("GET", f"/guild/{guild_id}/stickers"))
+        return await self.request(Route("POST", f"/guild/{guild_id}/stickers"), data=payload, reason=reason)
+
+    async def modify_guild_sticker(self, payload: dict, guild_id: "Snowflake_Type", sticker_id: "Snowflake_Type", reason: Optional[str] = None):
+        """
+        Modify the given sticker. Requires the MANAGE_EMOJIS_AND_STICKERS permission.
+
+        :param payload: the payload to send.
+        :param guild_id: The guild of the target sticker.
+        :param sticker_id:  The sticker to modify.
+        :param reason: The reason for this action.
+
+        :return: The updated sticker data on success.
+        """
+        return await self.request(Route("PATCH", f"/guild/{guild_id}/stickers/{sticker_id}"), data=payload, reason=reason)
+
+    async def delete_guild_sticker(self, guild_id: "Snowflake_Type", sticker_id: "Snowflake_Type", reason: Optional[str] = None) -> None:
+        """
+        Delete the given sticker. Requires the MANAGE_EMOJIS_AND_STICKERS permission.
+
+        :param guild_id: The guild of the target sticker.
+        :param sticker_id:  The sticker to delete.
+        :param reason: The reason for this action.
+
+        :return: Returns 204 No Content on success.
+        """
+        return await self.request(Route("DELETE", f"/guild/{guild_id}/stickers/{sticker_id}"), reason=reason)
