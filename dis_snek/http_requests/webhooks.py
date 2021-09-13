7570c684333bb271cf72aedea763cab61cd1dcff
LordOfPolls <ddavidallen13@gmail.com>
2021-09-13 21:11:27 +0100

    webhook message endpoints

--- dis_snek/http_requests/webhooks.py
+++ dis_snek/http_requests/webhooks.py
@@ -86,3 +86,69 @@ class WebhookRequests:
         endpoint = f"/webhooks/{webhook_id}{f'/{webhook_token}' if webhook_token else ''}"
 
         return await self.request(Route("DELETE", endpoint))
+
+    async def execute_webhook(
+        self,
+        webhook_id: "Snowflake_Type",
+        webhook_token: str,
+        payload: dict,
+        wait=False,
+        thread_id: "Snowflake_Type" = None,
+    ) -> dict:
+        """
+        Execute a webhook. Basically send a message as the webhook.
+
+        :param webhook_id: The ID of the webhook to delete
+        :param webhook_token: The token for the webhook
+        :param payload: The JSON payload for the message
+        :param wait: Waits for server confirmation of message send before response
+        :param thread_id: Send a message to the specified thread
+        :param suffix: An optional suffix to add to the end of the endpoint address
+        :return: The sent `message`, if `wait` is True else None
+        """
+        return await self.request(
+            Route("POST", f"/webhooks/{webhook_id}/{webhook_token}"),
+            params={"wait": wait, "thread_id": thread_id},
+            data=payload,
+        )
+
+    async def get_webhook_message(
+        self, webhook_id: "Snowflake_Type", webhook_token: str, message_id: "Snowflake_Type"
+    ) -> dict:
+        """
+        Returns a previously-sent webhook message from the same token. Returns a message object on success.
+
+        :param webhook_id: The ID of the webhook to delete
+        :param webhook_token: The token for the webhook
+        :param message_id: The ID of a message sent by this webhook
+        :return: A message object on success
+        """
+        return await self.request(Route("GET", f"/webhooks/{webhook_id}/{webhook_token}/messages/{message_id}"))
+
+    async def edit_webhook_message(
+        self, webhook_id: "Snowflake_Type", webhook_token: str, message_id: "Snowflake_Type", payload: dict
+    ) -> dict:
+        """
+        Edits a previously-sent webhook message from the same token.
+
+        :param webhook_id: The ID of the webhook to delete
+        :param webhook_token: The token for the webhook
+        :param message_id: The ID of a message sent by this webhook
+        :param payload: The JSON payload for the message
+        :return: The updated message on success
+        """
+        return await self.request(
+            Route("PATCH", f"/webhooks/{webhook_id}/{webhook_token}/messages/{message_id}"), data=payload
+        )
+
+    async def delete_webhook_message(
+        self, webhook_id: "Snowflake_Type", webhook_token: str, message_id: "Snowflake_Type"
+    ) -> None:
+        """
+        Delete a message that was created by the same token.
+
+        :param webhook_id: The ID of the webhook to delete
+        :param webhook_token: The token for the webhook
+        :param message_id: The ID of a message sent by this webhook
+        """
+        return await self.request(Route("DELETE", f"/webhooks/{webhook_id}/{webhook_token}/messages/{message_id}"))
