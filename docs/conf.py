d1a01d2485f75475eafc0dcd7d0c092a3ee6aef7
LordOfPolls <ddavidallen13@gmail.com>
2021-09-14 20:20:07 +0100

    y'all asked for a theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ version = ".".join(__version__.split(".", 2)[:3])
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -58,7 +58,10 @@ exclude_patterns = ["_build", ""]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+
+html_theme = "insegel"
+html_logo = "_static/logo.png"
+html_theme_options = {"navigation_depth": 3}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
