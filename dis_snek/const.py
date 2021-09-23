39a58121c7230a4e074031c8ad1288d37118946e
LordOfPolls <ddavidallen13@gmail.com>
2021-09-23 17:05:09 +0100

    MISSING now always returns None

--- dis_snek/const.py
+++ dis_snek/const.py
@@ -61,5 +61,6 @@ MISSING = sentinel.create(
     cls_dict={
         "__eq__": lambda x, y: x.__name__ == y.__name__ if hasattr(y, "__name__") else False,
         "__name__": "MISSING",
+        "__getattr__": lambda x, y: None,
     }
 )
