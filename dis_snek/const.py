c3b0b1faccf23e8c36e8c25c2480734260fe25d5
LordOfPolls <ddavidallen13@gmail.com>
2021-10-06 12:05:21 +0100

    sentinel is no longer `truthy`

--- dis_snek/const.py
+++ dis_snek/const.py
@@ -62,5 +62,6 @@ MISSING = sentinel.create(
         "__eq__": lambda x, y: x.__name__ == y.__name__ if hasattr(y, "__name__") else False,
         "__name__": "MISSING",
         "__getattr__": lambda x, y: None,
+        "__bool__": lambda _: False,
     }
 )
