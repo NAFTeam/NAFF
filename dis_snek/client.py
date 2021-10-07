e308d6b01c3543718851b926698b8c6ff55a8f20
LordOfPolls <ddavidallen13@gmail.com>
2021-10-07 07:22:31 +0100

    Patch message weirdness

--- dis_snek/client.py
+++ dis_snek/client.py
@@ -611,6 +611,21 @@ class Snake:
             event: raw message event
         """
         msg = self.cache.place_message_data(event.data)
+        if not msg._guild_id and event.data["guild_id"]:
+            msg._guild_id = event.data["guild_id"]
+            # todo: Determine why this isn't set *always*
+
+        if not msg.author:
+            # sometimes discord will only send an author ID, not the author. this catches that
+            msg.channel = (
+                await self.cache.get_channel(to_snowflake(msg._channel_id)) if not msg.channel else msg.channel
+            )
+            if msg._guild_id:
+                msg.guild = await self.cache.get_guild(msg._guild_id) if not msg.guild else msg.guild
+                msg.author = await self.cache.get_member(msg._guild_id, msg._author_id)
+            else:
+                msg.author = await self.cache.get_user(to_snowflake(msg._author_id))
+
         self.dispatch(events.MessageCreate(msg))
 
     @listen()
