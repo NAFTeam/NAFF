140c94aeab00597d0085b81e7bad577c846574db
LordOfPolls <ddavidallen13@gmail.com>
2021-10-10 20:50:41 +0100

    Error handle auto population

--- dis_snek/models/discord.py
+++ dis_snek/models/discord.py
@@ -18,25 +18,34 @@ class ClientObject(DictSerializationMixin):
     def __attrs_post_init__(self):
         # This automatically populates the object with data from the cache
 
-        if hasattr(self, "channel") and self.channel is None and getattr(self, "_channel_id", None):
-            # If a channel attribute is expected, but not populated, grab it from the cache
-            self.channel = self._client.cache.channel_cache.get(int(self._channel_id))
+        try:
+            if hasattr(self, "channel") and self.channel is None and getattr(self, "_channel_id", None):
+                # If a channel attribute is expected, but not populated, grab it from the cache
+                self.channel = self._client.cache.channel_cache.get(int(self._channel_id))
 
-            if self.channel and getattr(self.channel, "_guild_id", None) and not getattr(self, "_guild_id", None):
-                # if we have a channel now, and are expecting a guild, but lack an ID, get it from the channel
-                self._guild_id = self.channel._guild_id
+                if self.channel and getattr(self.channel, "_guild_id", None) and not getattr(self, "_guild_id", None):
+                    # if we have a channel now, and are expecting a guild, but lack an ID, get it from the channel
+                    self._guild_id = self.channel._guild_id
+        except AttributeError:
+            pass
 
-        if hasattr(self, "guild") and self.guild is None and getattr(self, "_guild_id", None):
-            # if a guild is expected, but not populated, grab it from cache
-            self.guild = self._client.cache.guild_cache.get(int(self._guild_id))
+        try:
+            if hasattr(self, "guild") and self.guild is None and getattr(self, "_guild_id", None):
+                # if a guild is expected, but not populated, grab it from cache
+                self.guild = self._client.cache.guild_cache.get(int(self._guild_id))
+        except AttributeError:
+            pass
 
-        if hasattr(self, "author") and self.author is None and getattr(self, "_author_id", None):
-            # if an author is expected, but not populated, grab it from the cache
-            if self.guild:
-                self.author = self._client.cache.member_cache.get((int(self._guild_id), int(self._author_id)))
-            if not self.author:
-                # fall back to a user object if no member available or applicable
-                self.author = self._client.cache.user_cache.get(int(self._author_id))
+        try:
+            if hasattr(self, "author") and self.author is None and getattr(self, "_author_id", None):
+                # if an author is expected, but not populated, grab it from the cache
+                if self.guild:
+                    self.author = self._client.cache.member_cache.get((int(self._guild_id), int(self._author_id)))
+                if not self.author:
+                    # fall back to a user object if no member available or applicable
+                    self.author = self._client.cache.user_cache.get(int(self._author_id))
+        except AttributeError:
+            pass
 
     @classmethod
     def _process_dict(cls, data: Dict[str, Any], client: "Snake") -> Dict[str, Any]:
