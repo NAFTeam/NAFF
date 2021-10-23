ec23643abb6222f394fff24377a478b4fc870410
LordOfPolls <ddavidallen13@gmail.com>
2021-10-23 07:59:47 +0100

    remaining channel events

--- dis_snek/event_processors/channel_events.py
+++ dis_snek/event_processors/channel_events.py
@@ -4,6 +4,7 @@ from dis_snek.const import logger_name
 from dis_snek.event_processors._template import EventMixinTemplate
 from dis_snek.models import listen, events
 from dis_snek.models.events import RawGatewayEvent
+from dis_snek.utils.converters import timestamp_converter
 
 log = logging.getLogger(logger_name)
 
@@ -11,24 +12,25 @@ log = logging.getLogger(logger_name)
 class ChannelEvents(EventMixinTemplate):
     @listen()
     async def _on_raw_channel_create(self, event: RawGatewayEvent) -> None:
-        """
-        Automatically cache a guild upon CHANNEL_CREATE event from gateway.
 
-        Args:
-            data: raw channel data
-        """
         channel = self.cache.place_channel_data(event.data)
         self.dispatch(events.ChannelCreate(channel))
 
     @listen()
     async def _on_raw_channel_delete(self, event: RawGatewayEvent) -> None:
-        """
-        Process raw channel deletions and dispatch a processed channel deletion event.
-
-        Args:
-            event: raw channel deletion event
-        """
         # for some reason this event returns the deleted object?
         # so we cache it regardless
         channel = self.cache.place_channel_data(event.data)
         self.dispatch(events.ChannelDelete(channel))
+
+    @listen()
+    async def _on_raw_channel_update(self, event: RawGatewayEvent) -> None:
+        channel = self.cache.place_channel_data(event.data)
+        self.dispatch(events.ChannelUpdate(channel))
+
+    @listen()
+    async def _on_raw_channel_pins_update(self, event: RawGatewayEvent) -> None:
+        channel = await self.cache.get_channel(event.data.get("channel_id"))
+        channel.last_pin_timestamp = timestamp_converter(event.data.get("last_pin_timestamp"))
+        self.cache.channel_cache[channel.id] = channel
+        self.dispatch(events.ChannelPinsUpdate(channel, channel.last_pin_timestamp))
