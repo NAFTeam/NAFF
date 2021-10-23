49030c701541f8503b7d452e8c8f04546b2343e5
LordOfPolls <ddavidallen13@gmail.com>
2021-10-23 08:40:29 +0100

    role events

--- dis_snek/event_processors/role_events.py
+++ dis_snek/event_processors/role_events.py
@@ -1,6 +1,7 @@
+import copy
 import logging
 
-from dis_snek.const import logger_name
+from dis_snek.const import logger_name, MISSING
 from dis_snek.event_processors._template import EventMixinTemplate
 from dis_snek.models import listen, events
 from dis_snek.models.events import RawGatewayEvent
@@ -11,12 +12,21 @@ log = logging.getLogger(logger_name)
 class RoleEvents(EventMixinTemplate):
     @listen()
     async def _on_raw_guild_role_create(self, event: RawGatewayEvent) -> None:
-        ...
+        g_id = event.data.get("guild_id")
+        role = self.cache.place_role_data(g_id, [event.data.get("role")])
+        self.dispatch(events.GuildRoleCreate(g_id, role[event.data["role"]["id"]]))
 
     @listen()
     async def _on_raw_guild_role_update(self, event: RawGatewayEvent) -> None:
-        ...
+        g_id = event.data.get("guild_id")
+        r_data = event.data.get("role")
+        before = copy.copy(await self.cache.get_role(g_id, r_data["id"], False) or MISSING)
+
+        after = self.cache.place_role_data(g_id, [r_data])
+        after = after[int(event.data["role"]["id"])]
+
+        self.dispatch(events.GuildRoleUpdate(g_id, before, after))
 
     @listen()
     async def _on_raw_guild_role_delete(self, event: RawGatewayEvent) -> None:
-        ...
+        self.dispatch(events.GuildRoleDelete(event.data.get("guild_id"), event.data.get("role_id")))
