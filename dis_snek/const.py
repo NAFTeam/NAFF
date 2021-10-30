0cacdfc175f1a86152de8a4c4231ac909efbf65a
LordOfPolls <ddavidallen13@gmail.com>
2021-10-30 08:54:09 +0100

    make const version dynamic (maybe)

--- dis_snek/const.py
+++ dis_snek/const.py
@@ -30,14 +30,14 @@ attributes:
 """
 
 import sys
-import sentinel
 from collections import defaultdict
+from importlib.metadata import version as _v
 
-import tomli
+import sentinel
 
 _ver_info = sys.version_info
 
-__version__ = "1.2.2"
+__version__ = _v("dis-snek")
 __repo_url__ = "https://github.com/LordOfPolls/dis_snek"
 __py_version__ = f"{_ver_info[0]}.{_ver_info[1]}"
 logger_name = "dis.snek"
