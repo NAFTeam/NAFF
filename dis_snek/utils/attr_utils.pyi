dbce1a39a0ed83db0d97c1d35685fdbbc27e25f0
Sonic4999 <sonictbm@gmail.com>
2021-11-02 16:11:22 -0400

    Properly typehint attr_utils

--- dis_snek/utils/attr_utils.pyi
+++ dis_snek/utils/attr_utils.pyi
@@ -1,10 +1,11 @@
 import attr
-from typing import Any, TypeVar, Callable, Tuple, Union
+import logging
+from typing import Any, TypeVar, Callable, Tuple, Union, Optional
 
 # this took way too lonk to solve
 # but this solution is based on https://www.attrs.org/en/stable/extending.html
 # or, well, more specifically, the pyright section
-# doing this in the actual file itself causes the function to return as a nonetype
+# doing this in the actual file itself causes the fucntion to return as a nonetype
 
 _T = TypeVar("_T")
 
@@ -15,5 +16,14 @@ def __dataclass_transform__(
     kw_only_default: bool = False,
     field_descriptors: Tuple[Union[type, Callable[..., Any]], ...] = (()),
 ) -> Callable[[_T], _T]: ...
+
+log: logging.Logger
+
+class_defaults: dict[str, bool | list[Callable]]
+field_defaults: dict[str, bool]
+
 @__dataclass_transform__(field_descriptors=(attr.attrib, attr.field))
-def define(**kwargs): ...
+def define(f: Optional[Callable] = None, **kwargs): ...
+def field(**kwargs) -> Any: ...
+def docs(doc_string: str) -> dict[str, str]: ...
+def str_validator(self, attribute: attr.Attribute, value: Any) -> None: ...
