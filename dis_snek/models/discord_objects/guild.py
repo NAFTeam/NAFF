d7bf6fa9e3a8cd181d1880228a63a3b9b9816b5d
Michael <kamisoidesu@protonmail.com>
2021-12-20 09:05:48 +0100

    Forgot to edit one `name` to `query`
    My bad ._.

--- dis_snek/models/discord_objects/guild.py
+++ dis_snek/models/discord_objects/guild.py
@@ -1018,7 +1018,7 @@ class Guild(BaseGuild):
         return [GuildIntegration.from_dict(d | {"guild_id": self.id}, self._client) for d in data]
 
     async def search_guild_members(self, query: str, limit: int = 1) -> List["Member"]:
-        data = await self._client.http.search_guild_members(guild_id=self.id, query=name, limit=limit)
+        data = await self._client.http.search_guild_members(guild_id=self.id, query=query, limit=limit)
         return [self._client.cache.place_member_data(self.id, _d) for _d in data]
 
 
