abc2eea8bcb98984be859fddbe382e663d7dea5a
leestarb <56854192+leestarb@users.noreply.github.com>
2022-01-30 13:47:12 +0600

    style: Black format

--- dis_snek/client/client.py
+++ dis_snek/client/client.py
@@ -408,7 +408,7 @@ class Snake(
         self._user = SnakeBotUser.from_dict(me, self)
         self.cache.place_user_data(me)
         self._app = Application.from_dict(await self.http.get_current_bot_information(), self)
-        self._mention_reg = re.compile(fr"^(<@!?{self.user.id}*>\s)")
+        self._mention_reg = re.compile(rf"^(<@!?{self.user.id}*>\s)")
         self.dispatch(events.Login())
 
         await self._connection_state.start()
