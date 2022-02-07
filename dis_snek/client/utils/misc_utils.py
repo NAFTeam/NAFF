2ed06d6abf6ffde569ac3bb8af9453ffff6146dc
zevaryx <57541873+zevaryx@users.noreply.github.com>
2022-02-07 00:12:29 -0700

    feat: Add get util (#243)
    * feat: Add get util
    
    * fix: Change logic to skip items with missing attrs
    
    * feat: Add find_all and get_all utility functions
    
    * fix: Use list literal instead of list()
    
    * fix: List comprehension on find_all for better performance
    
    Co-authored-by: LordOfPolls <22540825+LordOfPolls@users.noreply.github.com>

--- dis_snek/client/utils/misc_utils.py
+++ dis_snek/client/utils/misc_utils.py
@@ -1,7 +1,7 @@
 import functools
 import inspect
 import re
-from typing import Callable, Iterable, Optional, Any
+from typing import Callable, Iterable, List, Optional, Any
 
 __all__ = ["escape_mentions", "find", "wrap_partial", "get_parameters"]
 
@@ -45,6 +45,80 @@ def find(predicate: Callable, sequence: Iterable) -> Optional[Any]:
         if predicate(el):
             return el
     return None
+
+
+def find_all(predicate: Callable, sequence: Iterable) -> List[Any]:
+    """
+    Find all elements in a sequence that match the predicate.
+
+    ??? Hint "Example Usage:"
+        ```python
+        members = find_all(lambda m: m.name == "UserName", guild.members)
+        ```
+    Args:
+        predicate: A callable that returns a boolean value
+        sequence: A sequence to be searched
+
+    Returns:
+        A list of matches
+
+    """
+    return [el for el in sequence if predicate(el)]
+
+
+def get(sequence: Iterable, **kwargs: Any) -> Optional[Any]:
+    """
+    Find the first element in a sequence that matches all attrs.
+
+    ??? Hint "Example Usage:"
+        ```python
+        channel = get(guild.channels, nsfw=False, category="General")
+        ```
+
+    Args:
+        sequence: A sequence to be searched
+        kwargs: Keyword arguments to search the sequence for
+
+    Returns:
+        A match if found, otherwise None
+    """
+    if not kwargs:
+        return sequence[0]
+
+    for el in sequence:
+        if any(not hasattr(el, attr) for attr in kwargs.keys()):
+            continue
+        if all(getattr(el, attr) == value for attr, value in kwargs.items()):
+            return el
+    return None
+
+
+def get_all(sequence: Iterable, **kwargs: Any) -> List[Any]:
+    """
+    Find all elements in a sequence that match all attrs.
+
+    ??? Hint "Example Usage:"
+        ```python
+        channels = get_all(guild.channels, nsfw=False, category="General")
+        ```
+
+    Args:
+        sequence: A sequence to be searched
+        kwargs: Keyword arguments to search the sequence for
+
+    Returns:
+        A list of matches
+    """
+    if not kwargs:
+        return sequence
+
+    matches = []
+    for el in sequence:
+        if any(not hasattr(el, attr) for attr in kwargs.keys()):
+            continue
+        if all(getattr(el, attr) == value for attr, value in kwargs.items()):
+            matches.append(el)
+    return matches
 
 
 def wrap_partial(obj, cls) -> Callable:
