52851cecf41774afa31eee2ac449b7ac69d4cd94
Aiden <aiden8green@gmail.com>
2022-02-21 22:02:10 -0500

    revert: "feat: Improve todict and its usage (#309)" (#320)
    This reverts commit 840b3f42247c14e3d267dfef60ba864294fc293a.

--- dis_snek/client/utils/serializer.py
+++ dis_snek/client/utils/serializer.py
@@ -19,9 +19,6 @@ def export_converter(converter) -> dict:
 
 
 def to_dict(inst) -> dict:
-    if isinstance(inst, dict):
-        return inst
-
     if (converter := getattr(inst, "as_dict", None)) is not None:
         return converter()
 
