a45705f2ae0b06f8d84aab82837a4d558e89e154
zevaryx <57541873+zevaryx@users.noreply.github.com>
2022-03-05 23:42:28 -0700

    fix: Allow standalone instances to use `guild.fetch_channel` (#347)
    * fix: Allow standalone instances to use `guild.fetch_channel`
    
    * refactor: Add `client.gateway_started` property
    
    * refactor: channel.guild.id -> channel._guild_id
    
    * refactor: Change `State.gateway_started` to an `asyncio.Event` object

--- dis_snek/api/gateway/state.py
+++ dis_snek/api/gateway/state.py
@@ -39,6 +39,9 @@ class ConnectionState:
     gateway_url: str = MISSING
     """The URL that the gateway should connect to."""
 
+    gateway_started: asyncio.Event = asyncio.Event()
+    """Event to check if the gateway has been started."""
+
     _shard_task: asyncio.Task | None = None
 
     @property
@@ -66,6 +69,8 @@ class ConnectionState:
         self.start_time = datetime.now()
         self._shard_task = asyncio.create_task(self._ws_connect())
 
+        self.gateway_started.set()
+
         # Historically this method didn't return until the connection closed
         # so we need to wait for the task to exit.
         await self._shard_task
@@ -79,6 +84,8 @@ class ConnectionState:
         if self._shard_task is not None:
             await self._shard_task
             self._shard_task = None
+
+        self.gateway_started.clear()
 
     async def _ws_connect(self) -> None:
         log.info("Attempting to initially connect to gateway...")
