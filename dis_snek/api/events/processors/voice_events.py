113ed51b4a2497eccdb248d81a918399e9d9af78
LordOfPolls <ddavidallen13@gmail.com>
2022-03-12 10:41:42 +0000

    fix: Handle user leaving vc after restart

--- dis_snek/api/events/processors/voice_events.py
+++ dis_snek/api/events/processors/voice_events.py
@@ -23,10 +23,11 @@ class VoiceEvents(EventMixinTemplate):
 
         self.dispatch(events.VoiceStateUpdate(before, after))
 
-        if after is None and before.user_id == self.user.id:
-            if vc := self.cache.get_bot_voice_state(before._guild_id):
-                if vc.ws._closed:
-                    self.cache.delete_bot_voice_state(before._guild_id)
+        if before:
+            if after is None and before.user_id == self.user.id:
+                if vc := self.cache.get_bot_voice_state(before._guild_id):
+                    if vc.ws._closed:
+                        self.cache.delete_bot_voice_state(before._guild_id)
 
     @Processor.define()
     async def on_raw_voice_server_update(self, event: "RawGatewayEvent") -> None:
