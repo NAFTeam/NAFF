b0bec3475e0a5101cd8a98f14e39d25861e5e42a
LordOfPolls <ddavidallen13@gmail.com>
2022-03-13 21:03:43 +0000

    feat: Audio buffering
    This is to prevent ffmpeg stuttering

--- dis_snek/api/voice/audio.py
+++ dis_snek/api/voice/audio.py
@@ -1,12 +1,13 @@
 import asyncio
 import audioop
-
-
 import subprocess  # noqa: S404
+import threading
+import time
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Union, Optional
 
+from _cffi_backend import buffer
 from yt_dlp import YoutubeDL
 
 ytdl = YoutubeDL(
@@ -24,6 +25,46 @@ ytdl = YoutubeDL(
         "source_address": "0.0.0.0",  # noqa: S104
     }
 )
+
+
+class AudioBuffer:
+    def __init__(self):
+        self._buffer = bytearray()
+        self._lock = threading.Lock()
+
+    def __len__(self) -> int:
+        return len(self._buffer)
+
+    def extend(self, data: bytes) -> None:
+        """
+        Extend the buffer with additional data.
+
+        Args:
+            data: The data to add
+        """
+        with self._lock:
+            self._buffer.extend(data)
+
+    def read(self, total_bytes: int) -> bytearray:
+        """
+        Read `total_bytes` bytes of audio from the buffer.
+
+        Should the buffer lack enough bytes, this will block until enough are available
+
+        Args:
+            total_bytes: Amount of bytes to read.
+
+        Returns:
+            Desired amount of bytes
+        """
+        while len(self._buffer) < total_bytes:
+            # buffer isn't populated, block until it is
+            time.sleep(0.02)
+
+        with self._lock:
+            view = memoryview(self._buffer)
+            self._buffer = bytearray(view[total_bytes:])
+            return bytearray(view[:total_bytes])
 
 
 class BaseAudio(ABC):
@@ -59,6 +100,14 @@ class Audio(BaseAudio):
 
     source: str
     """The source ffmpeg should use to play the audio"""
+    process: subprocess.Popen
+    """The ffmpeg process to use"""
+    buffer: AudioBuffer
+    """The audio objects buffer to prevent stuttering"""
+    buffer_seconds: int
+    """How many seconds of audio should be buffered"""
+    read_ahead_task: threading.Thread
+    """A thread that reads ahead to create the buffer"""
 
     def __init__(self, src: Union[str, Path]):
         self.source = src
@@ -66,29 +115,52 @@ class Audio(BaseAudio):
         self.locked_stream = False
         self.process: Optional[subprocess.Popen] = None
 
+        self.buffer = AudioBuffer()
+
+        self.buffer_seconds = 3
+        self.read_ahead_task = threading.Thread(target=self._read_ahead, daemon=True)
+
     def __repr__(self):
         return f"<{type(self).__name__}: {self.source}>"
 
+    @property
+    def _max_buffer_size(self):
+        # 1ms of audio * (buffer seconds * 1000)
+        return 192 * (self.buffer_seconds * 1000)
+
     def _create_process(self):
-        cmd = f"ffmpeg -i {self.source} -f s16le -ar 48000 -ac 2 -loglevel warning pipe:1"
+        cmd = f"ffmpeg -i {self.source} -f s16le -ar 48000 -ac 2 -loglevel warning pipe:1 -vn"
         self.process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: S603
+        self.read_ahead_task.start()
+
+    def _read_ahead(self) -> None:
+        while self.process:
+            if not len(self.buffer) > self._max_buffer_size:
+                self.buffer.extend(self.process.stdout.read(3840))
+            else:
+                time.sleep(0.1)
+        # reset buffer as soon as the process ends
+        self.buffer = AudioBuffer()
 
     def read(self, frame_size: int) -> bytes:
         """
-        Reads frame_size ms of audio from source.
+        Reads frame_size bytes of audio from the buffer.
 
         returns:
             bytes of audio
         """
         if not self.process:
             self._create_process()
-        data = self.process.stdout.read(frame_size)
+
+        data = self.buffer.read(frame_size)
+
         if len(data) != frame_size:
             data = b""
+
         return data
 
     def cleanup(self) -> None:
-        """Cleans up subprocess."""
+        """Cleans up after this audio object."""
         if self.process:
             self.process.kill()
 
