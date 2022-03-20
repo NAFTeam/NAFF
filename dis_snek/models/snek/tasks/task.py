50fed8bb692bee1ee9a124762fef7c819a1b34ff
LordOfPolls <ddavidallen13@gmail.com>
2022-03-20 10:54:22 +0000

    feat: More descript error when starting task without event loop
    Response to #240

--- dis_snek/models/snek/tasks/task.py
+++ dis_snek/models/snek/tasks/task.py
@@ -83,8 +83,13 @@ class Task:
 
     def start(self) -> None:
         """Start this task."""
-        self._stop.clear()
-        self.task = asyncio.create_task(self._task_loop())
+        try:
+            self._stop.clear()
+            self.task = asyncio.create_task(self._task_loop())
+        except RuntimeError:
+            log.error(
+                "Unable to start task without a running event loop! We recommend starting tasks within an `on_startup` event."
+            )
 
     def stop(self) -> None:
         """End this task."""
