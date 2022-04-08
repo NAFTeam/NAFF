57cb4ffb4526a5ce8ef8600b4927741e9622a9ff
LordOfPolls <ddavidallen13@gmail.com>
2022-04-08 21:18:59 +0100

    feat: Add decryption class

--- dis_snek/api/voice/encryption.py
+++ dis_snek/api/voice/encryption.py
@@ -1,4 +1,6 @@
-__all__ = ["Encryption"]
+__all__ = ["Encryption", "Decryption"]
+
+from abc import ABC, abstractmethod
 
 try:
     from nacl import secret, utils
@@ -8,9 +10,8 @@ except ImportError:
     nacl_imported = False
 
 
-class Encryption:
+class Crypt(ABC):
     SUPPORTED = (
-        # "xsalsa20_poly1305_lite",
         "xsalsa20_poly1305_suffix",
         "xsalsa20_poly1305",
     )
@@ -20,20 +21,24 @@ class Encryption:
             raise RuntimeError("Please install dis-snek[voice] to use voice components.")
         self.box: secret.SecretBox = secret.SecretBox(bytes(secret_key))
 
+    @abstractmethod
+    def xsalsa20_poly1305_suffix(self, header: bytes, data) -> bytes:
+        raise NotImplementedError
+
+    @abstractmethod
+    def xsalsa20_poly1305(self, header: bytes, data) -> bytes:
+        raise NotImplementedError
+
+
+class Encryption(Crypt):
     def encrypt(self, mode: str, header: bytes, data) -> bytes:
         match mode:
-            # case "xsalsa20_poly1305_lite":
-            #     return self.xsalsa20_poly1305_lite(header, data)
             case "xsalsa20_poly1305_suffix":
                 return self.xsalsa20_poly1305_suffix(header, data)
             case "xsalsa20_poly1305":
                 return self.xsalsa20_poly1305(header, data)
             case _:
                 raise RuntimeError(f"Unsupported encryption type requested: {mode}")
-
-    def xsalsa20_poly1305_lite(self, header: bytes, data) -> bytes:
-        # todo: hi!
-        ...
 
     def xsalsa20_poly1305_suffix(self, header: bytes, data) -> bytes:
         nonce = utils.random(secret.SecretBox.NONCE_SIZE)
@@ -44,3 +49,25 @@ class Encryption:
         nonce[:12] = header
 
         return header + self.box.encrypt(bytes(data), bytes(nonce)).ciphertext
+
+
+class Decryption(Crypt):
+    def decrypt(self, mode: str, header: bytes, data) -> bytes:
+        match mode:
+            case "xsalsa20_poly1305_suffix":
+                return self.xsalsa20_poly1305_suffix(header, data)
+            case "xsalsa20_poly1305":
+                return self.xsalsa20_poly1305(header, data)
+            case _:
+                raise RuntimeError(f"Unsupported decryption type requested: {mode}")
+
+    def xsalsa20_poly1305_suffix(self, header: bytes, data) -> bytes:
+        nonce = data[-24:]
+
+        return self.box.decrypt(bytes(data[:-24]), bytes(nonce))
+
+    def xsalsa20_poly1305(self, header: bytes, data) -> bytes:
+        nonce = bytearray(24)
+        nonce[:12] = header
+
+        return self.box.decrypt(bytes(data), bytes(nonce))
