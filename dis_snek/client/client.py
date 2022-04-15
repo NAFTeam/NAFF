883a4070668e3f7d3e393d9c607087755a7f708b
LordOfPolls <22540825+LordOfPolls@users.noreply.github.com>
2022-04-15 14:13:26 +0100

    feat: support multi-scale unloading (#441)
    * feat💥: support multiple scales in one extension file
    
    * feat💥: sssss
    
    * fix: update shed_scale
    
    * feat: restore get_scale

--- dis_snek/client/client.py
+++ dis_snek/client/client.py
@@ -1504,22 +1504,38 @@ class Snake(
     async def _disconnect(self) -> None:
         self._ready.clear()
 
-    def get_scale(self, name) -> Optional[Scale]:
+    def get_scales(self, name: str) -> list[Scale]:
         """
-        Get a scale.
+        Get all scales with a name or extension name.
 
         Args:
             name: The name of the scale, or the name of it's extension
 
         Returns:
-            Scale or None if no scale is found
+            List of Scales
         """
+        out = []
         if name not in self.scales.keys():
             for scale in self.scales.values():
                 if scale.extension_name == name:
-                    return scale
+                    out.append(scale)
+            return out
 
-        return self.scales.get(name, None)
+        return [self.scales.get(name, None)]
+
+    def get_scale(self, name: str) -> Scale | None:
+        """
+        Get a scale with a name or extension name.
+
+        Args:
+            name: The name of the scale, or the name of it's extension
+
+        Returns:
+            A scale, if found
+        """
+        if scales := self.get_scales(name):
+            return scales[0]
+        return None
 
     def grow_scale(self, file_name: str, package: str = None, **load_kwargs) -> None:
         """
@@ -1542,8 +1558,8 @@ class Snake(
             unload_kwargs: The auto-filled mapping of the unload keyword arguments
 
         """
-        if scale := self.get_scale(scale_name):
-            return self.unload_extension(inspect.getmodule(scale).__name__, **unload_kwargs)
+        if scale := self.get_scales(scale_name):
+            return self.unload_extension(inspect.getmodule(scale[0]).__name__, **unload_kwargs)
 
         raise ScaleLoadException(f"Unable to shed scale: No scale exists with name: `{scale_name}`")
 
@@ -1622,7 +1638,7 @@ class Snake(
         except AttributeError:
             pass
 
-        if scale := self.get_scale(name):
+        for scale in self.get_scales(name):
             scale.shed(**unload_kwargs)
 
         del sys.modules[name]
