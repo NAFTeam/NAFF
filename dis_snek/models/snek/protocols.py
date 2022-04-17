b9cfd75b11d63c2d94f7283d95e0f4420e4b9a7c
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2022-04-17 19:47:53 -0400

    docs: add docstrings for Converter

--- dis_snek/models/snek/protocols.py
+++ dis_snek/models/snek/protocols.py
@@ -11,5 +11,19 @@ __all__ = ["Converter"]
 
 @typing.runtime_checkable
 class Converter(Protocol[T_co]):
+    """A protocol representing a class used to convert an argument."""
+
     async def convert(self, ctx: "Context", argument: Any) -> T_co:
+        """
+        The function that converts an argument to the appropriate type.
+
+        This should be overridden by subclasses for their conversion logic.
+
+        Args:
+            ctx (Context): The context to use for the conversion.
+            argument (Any): The argument to be converted.
+
+        Returns:
+            Any: The converted argument.
+        """
         raise NotImplementedError("Derived classes need to implement this.")
