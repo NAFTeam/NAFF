6f09f3b8e788202e0422f357b34c0513b5b48587
james <41456914+jameswalston@users.noreply.github.com>
2022-05-13 03:23:28 -0400

    feat: expose `Application` attrs for App Discovery (#476)
    * feat: expose `Application` attrs for App Discovery
    
    This simply exposes the `tags`, `install_params` and `custom_install_url` which were recently amended to the `Application` object when application discovery was announced. These also may have just been simply overlooked
    
    * feat: add `install_params` attr
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: LordOfPolls <ddavidallen13@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- naff/models/discord/application.py
+++ naff/models/discord/application.py
@@ -54,6 +54,13 @@ class Application(DiscordObject):
     """The application's default rich presence invite cover"""
     flags: Optional["ApplicationFlags"] = field(default=None, converter=optional(ApplicationFlags))
     """The application's public flags"""
+    tags: Optional[List[str]] = field(default=None)
+    """The application's tags describing its functionality and content"""
+    # todo: implement an ApplicationInstallParams object. See https://discord.com/developers/docs/resources/application#install-params-object
+    install_params: Optional[dict] = field(default=None)
+    """The application's settings for in-app invitation to guilds"""
+    custom_install_url: Optional[str] = field(default=None)
+    """The application's custom authorization link for invitation to a guild"""
 
     @classmethod
     def _process_dict(cls, data: Dict[str, Any], client: "Client") -> Dict[str, Any]:
