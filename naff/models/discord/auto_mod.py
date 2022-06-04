fa421e41e54e4b1e521334ab4a90990a2e687125
LordOfPolls <ddavidallen13@gmail.com>
2022-06-04 21:46:57 +0100

    feat: create auto mod rule object

--- naff/models/discord/auto_mod.py
+++ naff/models/discord/auto_mod.py
@@ -3,13 +3,13 @@ from typing import TYPE_CHECKING, Union
 
 from naff.client.const import logger_name, MISSING
 from naff.client.utils.attr_utils import define, field
-from naff.models.discord.base import ClientObject
-from naff.models.discord.enums import AutoModTriggerType, AutoModAction
+from naff.models.discord.base import ClientObject, DiscordObject
+from naff.models.discord.enums import AutoModTriggerType, AutoModAction, AutoModEvent
 
 if TYPE_CHECKING:
-    from naff import Snowflake_Type, Guild, GuildText, Message, Client
+    from naff import Snowflake_Type, Guild, GuildText, Message, Client, Member
 
-__all__ = ("AutoModerationAction",)
+__all__ = ("AutoModerationAction", "AutoModRule")
 
 log = logging.getLogger(logger_name)
 
@@ -26,6 +26,37 @@ class BaseAction(ClientObject):
             action_class = cls
 
         return action_class.from_dict({"type": data.get("type")} | data["metadata"], client)
+
+
+@define()
+class AutoModRule(DiscordObject):
+    name: str = field()
+    enabled: bool = field(default=False)
+
+    actions: list[BaseAction] = field(factory=list)
+    event_type: AutoModEvent = field()
+    trigger_type: AutoModTriggerType = field()
+    trigger_metadata: dict = field()
+
+    exempt_roles: list["Snowflake_Type"] = field(factory=list)
+    exempt_channels: list["Snowflake_Type"] = field(factory=list)
+
+    _guild_id: "Snowflake_Type" = field()
+    _creator_id: "Snowflake_Type" = field()
+
+    @classmethod
+    def _process_dict(cls, data: dict, client: "Client") -> dict:
+        data = super()._process_dict(data, client)
+        data["actions"] = [BaseAction.from_dict_factory(d, client) for d in data["actions"]]
+        return data
+
+    @property
+    def creator(self) -> "Member":
+        return self._client.cache.get_member(self._guild_id, self._creator_id)
+
+    @property
+    def guild(self) -> "Guild":
+        return self._client.cache.get_guild(self._guild_id)
 
 
 @define()
