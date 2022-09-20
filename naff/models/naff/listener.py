b1147697e6d5a0d3d3e81bdf073c45eac684b048
Daniel J <38250010+Kigstn@users.noreply.github.com>
2022-09-20 21:18:58 +0200

    feat💥: Switch error and completion function to proper events (#641)
    * feat 💥: delay non-raw listeners until client ready (#635)
    
    * feat 💥: delay non-raw listeners until client ready
    
    * fix: dont delay error events
    
    * refactor 💥: Update and organise event bases (#636)
    
    * feat: allow listeners to be deleted if they are marked as overwritable
    
    * feat!: convert error and completion handlers to events, rename the completion events and add/rename modal events
    
    * refactor: rename `delete_if_overwritten` to `delete_if_overridden`
    
    Co-authored-by: LordOfPolls <dev@lordofpolls.com>

--- naff/models/naff/listener.py
+++ naff/models/naff/listener.py
@@ -16,19 +16,33 @@ class Listener(CallbackObject):
     """Name of the event to listen to."""
     callback: Coroutine
     """Coroutine to call when the event is triggered."""
+    delete_if_overridden: bool
+    """Should the listener be deleted if another listener is added for the same event. Used for builtin error events that can be overwritten by advanced users"""
     delay_until_ready: bool
     """whether to delay the event until the client is ready"""
 
-    def __init__(self, func: Callable[..., Coroutine], event: str, *, delay_until_ready: bool = True) -> None:
+    def __init__(
+        self,
+        func: Callable[..., Coroutine],
+        event: str,
+        *,
+        delay_until_ready: bool = True,
+        delete_if_overridden: bool = False,
+    ) -> None:
         super().__init__()
 
         self.event = event
         self.callback = func
         self.delay_until_ready = delay_until_ready
+        self.delete_if_overridden = delete_if_overridden
 
     @classmethod
     def create(
-        cls, event_name: Absent[str | BaseEvent] = MISSING, *, delay_until_ready: bool = True
+        cls,
+        event_name: Absent[str | BaseEvent] = MISSING,
+        *,
+        delay_until_ready: bool = True,
+        delete_if_overridden: bool = False,
     ) -> Callable[[Coroutine], "Listener"]:
         """
         Decorator for creating an event listener.
@@ -36,6 +50,7 @@ class Listener(CallbackObject):
         Args:
             event_name: The name of the event to listen to. If left blank, event name will be inferred from the function name or parameter.
             delay_until_ready: Whether to delay the listener until the client is ready.
+            delete_if_overridden: Should the listener be deleted if another listener is added for the same event. Used for builtin error events that can be overwritten by advanced users
 
         Returns:
             A listener object.
@@ -61,13 +76,21 @@ class Listener(CallbackObject):
                 if not name:
                     name = coro.__name__
 
-            return cls(coro, get_event_name(name), delay_until_ready=delay_until_ready)
+            return cls(
+                coro,
+                get_event_name(name),
+                delay_until_ready=delay_until_ready,
+                delete_if_overridden=delete_if_overridden,
+            )
 
         return wrapper
 
 
 def listen(
-    event_name: Absent[str | BaseEvent] = MISSING, *, delay_until_ready: bool = True
+    event_name: Absent[str | BaseEvent] = MISSING,
+    *,
+    delay_until_ready: bool = True,
+    delete_if_overridden: bool = False,
 ) -> Callable[[Callable[..., Coroutine]], Listener]:
     """
     Decorator to make a function an event listener.
@@ -75,9 +98,10 @@ def listen(
     Args:
         event_name: The name of the event to listen to. If left blank, event name will be inferred from the function name or parameter.
         delay_until_ready: Whether to delay the listener until the client is ready.
+        delete_if_overridden: Should the listener be deleted if another listener is added for the same event. Used for builtin error events that can be overwritten by advanced users
 
     Returns:
         A listener object.
 
     """
-    return Listener.create(event_name, delay_until_ready=delay_until_ready)
+    return Listener.create(event_name, delay_until_ready=delay_until_ready, delete_if_overridden=delete_if_overridden)
