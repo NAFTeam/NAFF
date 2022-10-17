f1adf189cbcbaf4d693ce854a05083349f9e0621
LordOfPolls <dev@lordofpolls.com>
2022-10-17 11:18:09 +0100

    feat: add additional voice events (#674)

--- naff/api/events/processors/voice_events.py
+++ naff/api/events/processors/voice_events.py
@@ -24,6 +24,18 @@ class VoiceEvents(EventMixinTemplate):
                 # noinspection PyProtectedMember
                 await vc._voice_state_update(before, after, event.data)
 
+        if before and after:
+            if (before.mute != after.mute) or (before.self_mute != after.self_mute):
+                self.dispatch(events.VoiceUserMute(after, after.member, after.channel, after.mute or after.self_mute))
+            if (before.deaf != after.deaf) or (before.self_deaf != after.self_deaf):
+                self.dispatch(events.VoiceUserDeafen(after, after.member, after.channel, after.deaf or after.self_deaf))
+            if before.channel != after.channel:
+                self.dispatch(events.VoiceUserMove(after, after.member, before.channel, after.channel))
+        elif not before and after:
+            self.dispatch(events.VoiceUserJoin(after, after.member, after.channel))
+        elif before and not after:
+            self.dispatch(events.VoiceUserLeave(before, before.member, before.channel))
+
     @Processor.define()
     async def _on_raw_voice_server_update(self, event: "RawGatewayEvent") -> None:
         if vc := self.cache.get_bot_voice_state(event.data["guild_id"]):
