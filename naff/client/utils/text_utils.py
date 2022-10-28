780aedb6e610c922df88e6f00510a0b67acf9220
AlbertUnruh <AlbertUnruh@pm.me>
2022-10-28 21:17:53 +0200

    fix circular import

--- naff/client/utils/text_utils.py
+++ naff/client/utils/text_utils.py
@@ -1,20 +1,19 @@
 import re
-
-from naff.models.discord import BaseChannel, BaseUser, Role
+import naff.models as models
 
 __all__ = ("mentions",)
 
 
-def mentions(text: str, query: str | re.Pattern[str] | BaseUser | BaseChannel | Role) -> bool:
+def mentions(text: str, query: "str | re.Pattern[str] | models.BaseUser | models.BaseChannel | models.Role") -> bool:
     """Checks whether a query is present in a text."""
     # don't use match/case witch type(query) since subclasses aren't checked
     if isinstance(query, str):
         return query in text
     elif isinstance(query, re.Pattern):
         return query.match(text) is not None
-    elif isinstance(query, BaseUser):
+    elif isinstance(query, models.BaseUser):
         return (query.mention in text) or (query.username in text)
-    elif isinstance(query, (BaseChannel, Role)):
+    elif isinstance(query, (models.BaseChannel, models.Role)):
         return (query.mention in text) or (query.name in text)
     else:
         return False
